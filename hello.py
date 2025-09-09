32c48e8fc5b6d3abfc60550337c2ceefe084a292
dharaneeshU <dharaneesh.u_25am@kgkite.ac.in>
2025-09-09 15:10:00 +0530

    Add program to compare two numbers

---
+++ hello.py
@@ -0,0 +1,12 @@
+a = int(input("enter a number: "))
+r = int(input("enter a number: "))
+
+if a>r:
+    print(f"{a} is greater than {r}")
+elif a<r:
+    print(f"{a} is less than {r}")
+
+else:
+    print("they are equal")
+
+
